9f89b1dd9a84f46b53152701f74d15c34a22c4a7
Hordii Moroz <gordya1564@gmail.com>
2023-09-04 20:13:57 +0300

    Добавьте файлы проекта.

---
+++ 2LessonMoroz/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Hello, World!");
+string LocalFun()
+{
+    string name = "GEPY";
+    string surname = "NONO";
+    string FullName = $"{name} {surname}";
+    return FullName;
+}
+Console.WriteLine(LocalFun());
